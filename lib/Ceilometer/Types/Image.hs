7ac41b2a3ab24ec3fb50f0dc5ea85e6b9104be58
Oswyn Brent <oswyn.brent@anchor.com.au>
2015-02-06 14:56:44 +1100

    Added the Image.hs file

---
+++ lib/Ceilometer/Types/Image.hs
@@ -0,0 +1,74 @@
+--
+-- Copyright © 2013-2015 Anchor Systems, Pty Ltd and Others
+--
+-- The code in this file, and the program it is a part of, is
+-- made available to you by its authors as open source software:
+-- you can redistribute it and/or modify it under the terms of
+-- the 3-clause BSD licence.
+--
+-- /Description/
+-- This module defines the Ceilometer Image type.
+--
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE MultiWayIf         #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TemplateHaskell    #-}
+
+module Ceilometer.Types.Image
+  ( -- * Fields
+    PFImageStatus(..), pfImageStatus
+  , PFImageVerb(..), pfImageVerb
+  , PDImage(..), pdImage
+  , imageStatus, imageVerb, imageVal
+  ) where
+
+import           Control.Applicative
+import           Control.Lens
+import           Data.Binary           (Word8)
+import           Data.Text             (Text)
+import           Data.Typeable
+
+import           Ceilometer.Types.Base
+import           Ceilometer.Types.TH
+import           Vaultaire.Types
+
+$(declarePF    "Image"
+              ("Status", ''Word8)
+            [ ("Active"       , 1)
+            , ("Saving"       , 2)
+            , ("Deleted"      , 3)
+            , ("Queued"       , 4)
+            , ("PendingDelete", 5)
+            , ("Killed"       , 6) ]
+            [ ''Show, ''Read, ''Eq, ''Bounded, ''Enum ])
+
+$(declarePF    "Image"
+              ("Verb", ''Word8)
+            [ ("Serve"   , 1)
+            , ("Update"  , 2)
+            , ("Upload"  , 3)
+            , ("Download", 4)
+            , ("Delete"  , 5) ]
+            [ ''Show, ''Read, ''Eq, ''Bounded, ''Enum ])
+
+data PDImage = PDImage
+  { _imageStatus   :: PFImageStatus
+  , _imageVerb     :: PFImageVerb
+  , _imageVal      :: PFValue32 }
+  deriving (Eq, Show, Read, Typeable)
+
+$(makeLenses ''PDImage)
+
+pdImage :: Prism' PRCompoundEvent PDImage
+pdImage = prism' pretty parse
+  where parse raw
+          =   PDImage
+          <$> (raw ^? eventStatus   . pfImageStatus)
+          <*> (raw ^? eventVerb     . pfImageVerb)
+          <*> (raw ^? eventVal )
+        pretty (PDImage status verb val)
+          = PRCompoundEvent
+            val
+            (Instant ^. re pfEndpoint)
+            (verb   ^. re pfImageVerb)
+            (status ^. re pfImageStatus)
